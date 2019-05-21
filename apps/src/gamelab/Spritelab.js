f4078406d0c82e8f60ac0b41e077c1b97e2b4707
Anjali <anjali@code.org>
2019-05-21 09:58:18 -0700

    getProp

--- apps/src/gamelab/Spritelab.js
+++ apps/src/gamelab/Spritelab.js
@@ -5,7 +5,7 @@ var Spritelab = function() {
   var allSpritesWithAnimation = function(animationName) {
     let group = [];
     Object.keys(nativeSpriteMap).forEach(s => {
-      if (nativeSpriteMap[s].animationName === animationName) {
+      if (nativeSpriteMap[s].getAnimationLabel() === animationName) {
         group.push(nativeSpriteMap[s]);
       }
     });
@@ -26,6 +26,24 @@ var Spritelab = function() {
     console.log('unknown type of spriteOrGroup: ' + typeof spriteOrGroup);
   };
 
+  window.p5.prototype.getProp = function(spriteIndex, prop) {
+    if (!spriteIndex) {
+      return undefined;
+    }
+    // Unclear how getters should function with groups. For now just use first element
+    // ^ Pulled from library. TODO- now seems like a good time to make a decision here?
+    let sprite = singleOrGroup(spriteIndex)[0];
+    if (prop === 'scale') {
+      return sprite.getScale() * 100;
+    } else if (prop === 'costume') {
+      return sprite.getAnimationLabel();
+    } else if (prop === 'y') {
+      return 400 - sprite.y;
+    } else {
+      return sprite[prop];
+    }
+  };
+
   window.p5.prototype.destroy = function(spriteIndex) {
     let sprites = singleOrGroup(spriteIndex);
     if (sprites) {
@@ -41,7 +59,6 @@ var Spritelab = function() {
     if (nativeSpriteMap) {
       nativeSpriteMap.forEach(sprite => {
         sprite.setAnimation(animation);
-        sprite.animationName = animation;
         sprite.scale /= sprite.baseScale;
         sprite.baseScale =
           100 /
@@ -60,6 +77,9 @@ var Spritelab = function() {
     nativeSpriteMap[spriteId] = sprite; // lookup sprite given id
     sprite.id = spriteId; // lookup id given sprite
     sprite.baseScale = 1;
+    sprite.getScale = function() {
+      return sprite.scale / sprite.baseScale;
+    };
     if (animation) {
       this.setAnimation(sprite.id, animation);
     }
